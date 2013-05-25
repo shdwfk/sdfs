fa1f95d359ae56d277e073c823f6d6bd7722727b
wangfk <fengkun.wang@gmail.com>
2013-05-25 22:07:15 +0800

    添加TestCaseGuiceModule，用于TestCase，描述Guice的注入映射关系

---
+++ SourceCode/src/test/java/org/sdfs/TestCaseGuiceModule.java
@@ -0,0 +1,30 @@
+package org.sdfs;
+
+import org.junit.Ignore;
+import org.sdfs.blockmaster.BlockMasterImpl;
+import org.sdfs.blockmaster.IBlockMaster;
+import org.sdfs.blockserver.BlockServerImpl;
+import org.sdfs.blockserver.IBlockServer;
+import org.sdfs.nameserver.INameServer;
+import org.sdfs.nameserver.NameServerImpl;
+import org.sdfs.superblock.ISuperBlock;
+import org.sdfs.superblock.SuperBlockImpl;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * 用于TestCase的Guice Module，描述Guice的注入映射关系
+ * @author wangfk
+ *
+ */
+@Ignore //通过Annotation表明这个类不是单元测试
+public class TestCaseGuiceModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		bind(ISuperBlock.class).to(SuperBlockImpl.class).asEagerSingleton();
+		bind(IBlockServer.class).to(BlockServerImpl.class).asEagerSingleton();
+		bind(IBlockMaster.class).to(BlockMasterImpl.class).asEagerSingleton();
+		bind(INameServer.class).to(NameServerImpl.class).asEagerSingleton();
+	}
+}
