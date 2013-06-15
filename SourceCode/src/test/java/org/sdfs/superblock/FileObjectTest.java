e08d02034b6ccbc91b218ecd616822c632b3291d
wangfk <fengkun.wang@gmail.com>
2013-06-15 21:49:16 +0800

    为SuperBlock和FileObject添加详细的单元测试用例，明确方法参数和使用方式

---
+++ SourceCode/src/test/java/org/sdfs/superblock/FileObjectTest.java
@@ -0,0 +1,139 @@
+package org.sdfs.superblock;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.onami.test.OnamiRunner;
+import org.apache.onami.test.annotation.GuiceModules;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sdfs.TestCaseBase;
+import org.sdfs.TestCaseGuiceModule;
+import org.sdfs.commons.FileMeta;
+import org.sdfs.exceptions.SdfsException;
+
+import com.google.inject.Inject;
+
+@RunWith(OnamiRunner.class)
+@GuiceModules(TestCaseGuiceModule.class)
+public class FileObjectTest extends TestCaseBase {
+	private final Random random = new Random(System.currentTimeMillis());
+
+	@Inject
+	private ISuperBlock superBlock;
+
+	@Test
+	public void testFileRW() throws SdfsException, IOException {
+		long fileKey = 1L;
+
+		IFileObject fileObject = superBlock.getFileObject(fileKey);
+
+		// 创建并写文件
+		int size = random.nextInt(1024 * 1024);
+		OutputStream os = fileObject.createFile(null);
+		MockInputStream mockInputStream = new MockInputStream(size);
+		int copySize = IOUtils.copy(mockInputStream, os);
+		assertEquals(size, copySize);
+		os.close();
+		mockInputStream.close();
+
+		// 再次写入文件，抛出异常
+		try {
+			os = fileObject.createFile(null);
+			assertTrue(false);
+		} catch (SdfsException e) {}
+
+		// 测试fileExists接口
+		assertTrue(fileObject.fileExists());
+
+		// 测试getFileSize接口
+		assertEquals(size, fileObject.getFileSize());
+
+		// 读文件并check
+		InputStream is = fileObject.openFile();
+		verifyInputStream(size, mockInputStream.getAdler32(), is);
+		is.close();
+
+		// 再次读取文件并check
+		is = fileObject.openFile();
+		verifyInputStream(size, mockInputStream.getAdler32(), is);
+		is.close();
+
+		// 读取 FileMeta，应该是null
+		assertNull(fileObject.getFileMeta());
+
+		// 更新 FileMeta
+		fileObject.updateFileMeta(new FileMeta());
+
+		// 读取 FileMeta，不再是null
+		assertNotNull(fileObject.getFileMeta());
+
+		// 删除文件
+		fileObject.delete();
+
+		// 测试fileExists接口
+		assertFalse(fileObject.fileExists());
+
+		// 测试getFileSize接口：对于不存在或者已删除文件，getFileSize返回0
+		assertEquals(0, fileObject.getFileSize());
+
+		// 已删除文件，读操作会抛出异常
+		try {
+			is = fileObject.openFile();
+			assertTrue(false);
+		} catch (SdfsException e) {}
+
+		// 已删除文件，取FileMeta会抛出异常
+		try {
+			fileObject.getFileMeta();
+			assertTrue(false);
+		} catch (SdfsException e) {}
+
+		// 已删除文件，可以再次创建并写文件
+		size = random.nextInt(1024 * 1024);
+		os = fileObject.createFile(null);
+		mockInputStream = new MockInputStream(size);
+		copySize = IOUtils.copy(mockInputStream, os);
+		assertEquals(size, copySize);
+		os.close();
+		mockInputStream.close();
+
+		// 再次读文件并check
+		is = fileObject.openFile();
+		verifyInputStream(size, mockInputStream.getAdler32(), is);
+		is.close();
+	}
+
+	public void testFileNotExists() throws SdfsException {
+		long fileKey = 2L;
+
+		IFileObject fileObject = superBlock.getFileObject(fileKey);
+
+		// 测试fileExists接口
+		assertFalse(fileObject.fileExists());
+
+		// 测试getFileMeta接口，会抛出异常
+		try {
+			fileObject.getFileMeta();
+			assertTrue(false);
+		} catch (SdfsException e) {}
+
+		// 测试getFileSize接口：对于不存在或者已删除文件，getFileSize返回0
+		assertEquals(0, fileObject.getFileSize());
+
+		// 测试updateFileMeta接口，会抛出异常
+		try {
+			fileObject.updateFileMeta(null);
+			assertTrue(false);
+		} catch (SdfsException e) {}
+
+		// 测试updateFileMeta接口，会抛出异常
+		try {
+			fileObject.openFile();
+			assertTrue(false);
+		} catch (SdfsException e) {}
+	}
+}
--- SourceCode/src/test/java/org/sdfs/superblock/SuperBlockTest.java
+++ SourceCode/src/test/java/org/sdfs/superblock/SuperBlockTest.java
@@ -1,11 +1,17 @@
 package org.sdfs.superblock;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.onami.test.OnamiRunner;
 import org.apache.onami.test.annotation.GuiceModules;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sdfs.TestCaseBase;
 import org.sdfs.TestCaseGuiceModule;
+import org.sdfs.commons.FileMeta;
 import org.sdfs.exceptions.SdfsException;
 
 import com.google.inject.Inject;
@@ -27,5 +33,79 @@ public class SuperBlockTest extends TestCaseBase {
 	public void testInject() throws SdfsException {
 		assertNotNull(superBlock);
 		assertEquals(SuperBlockImpl.class, superBlock.getClass());
+	}
+
+	@Test
+	public void testSuperBlock() throws SdfsException, IOException {
+		long key1 = 100L;
+		long key2 = 200L;
+		long key3 = 300L;
+
+		long size1 = 10000;
+		long size2 = 0;
+
+		// 使用key1创建一个文件对象
+		IFileObject fileObject1 = superBlock.getFileObject(key1);
+		// 创建文件，打开OutputStream；PS，FileMeta是可以传null的
+		OutputStream os = fileObject1.createFile(null);
+		// 写入文件
+		IOUtils.copy(new MockInputStream(size1), os);
+		// 关闭文件，文件成功创建
+		os.close();
+
+		// 文件数应该等于已经成功创建的文件数
+		assertEquals(1, superBlock.getFileCount());
+		// getAvailableSize返回值应该是所有文件长度的总和
+		assertEquals(size1, superBlock.getAvailableSize());
+		// getTotalSize返回值应该是包括已删除文件长度的总和
+		assertEquals(size1, superBlock.getTotalSize());
+
+		IFileObject fileObject2 = superBlock.getFileObject(key2);
+		// 文件数应该等于已经成功创建的文件数，key2还没有创建，不能算数
+		assertEquals(1, superBlock.getFileCount());
+		// 创建文件，打开OutputStream；PS，FileMeta是可以传null的
+		os = fileObject2.createFile(new FileMeta());
+		// 写入文件，文件的长度可以为0
+		IOUtils.copy(new MockInputStream(size2), os);
+		// 关闭文件，文件成功创建
+		os.close();
+
+		// 文件数应该等于已经成功创建的文件数
+		assertEquals(2, superBlock.getFileCount());
+		// getAvailableSize返回值应该是所有文件长度的总和
+		assertEquals(size1 + size2, superBlock.getAvailableSize());
+		// getTotalSize返回值应该是包括已删除文件长度的总和
+		assertEquals(size1 + size2, superBlock.getTotalSize());
+
+		// 删除key1的文件
+		fileObject1.delete();
+		// 文件数不包含已删除的文件
+		assertEquals(1, superBlock.getFileCount());
+		// getAvailableSize返回值应该是所有文件长度的总和
+		assertEquals(size2, superBlock.getAvailableSize());
+		// getTotalSize返回值应该是包括已删除文件长度的总和
+		assertEquals(size1 + size2, superBlock.getTotalSize());
+
+		superBlock.compact();
+		// compact之后，getTotalSize不包括已删除文件
+		assertEquals(size2, superBlock.getTotalSize());
+
+		// 删除key3的文件
+		IFileObject fileObject3 = superBlock.getFileObject(key3);
+		assertFalse(fileObject3.fileExists());
+		try {
+			// 尝试读取不存在的文件，会抛出SdfsException异常
+			fileObject3.openFile();
+			assertTrue(false);
+		} catch (SdfsException e) {
+			
+		}
+		try {
+			// 删除不存在的文件，会抛出SdfsException异常
+			fileObject3.delete();
+			assertTrue(false);
+		} catch (SdfsException e) {
+			
+		}
 	}
 }
